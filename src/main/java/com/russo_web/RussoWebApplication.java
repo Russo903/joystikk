b8817c83b9b7928412e36ef3b77532addf144229
Gino Russo <grusso215@gmail.com>
2025-01-21 11:40:44 -0500

    first commit

---
+++ src/main/java/com/russo_web/RussoWebApplication.java
@@ -0,0 +1,13 @@
+package com.russo_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RussoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RussoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/russo_web/RussoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.russo_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RussoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
